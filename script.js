ff786bdf53e40e105a9df20630033124c4a2bdd6
vitacodesource <vitaaguitar@gmail.com>
2026-05-17 16:13:16 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255, 0, 0); // should permanently show red
-}
-
-function draw() {
-  fill(0);
-  ellipse(mouseX, mouseY, 50);
-}
