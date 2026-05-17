dfda7e3264ee941b9aec9541cfcb954c278ed731
vitacodesource <vitaaguitar@gmail.com>
2026-05-17 16:30:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,23 +8,11 @@ function draw() {
 
   let t = mouseX / width;
 
-  let c1 = color(10, 20, 50, 120);
-  let c2 = color(200, 120, 80, 120);
-  let c3 = color(255, 220, 150, 120);
+  let r = lerp(0, 255, t);
+  let g = lerp(0, 180, t);
+  let b = lerp(255, 80, t);
 
-  let col;
+  fill(r, g, b, 180);
 
-  if (t < 0.33) {
-    col = c1;
-  } else if (t < 0.66) {
-    col = c2;
-  } else {
-    col = c3;
-  }
-
-  fill(col);
-
-  let size = t < 0.33 ? 120 : t < 0.66 ? 180 : 240;
-
-  ellipse(mouseX, mouseY, size);
+  ellipse(mouseX, mouseY, 180);
 }
