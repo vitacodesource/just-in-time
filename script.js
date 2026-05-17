f4d6ab87579d7bf8e934cc029a5632098c2e4513
vitacodesource <vitaaguitar@gmail.com>
2026-05-17 18:39:15 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ function setup() {
 }
 
 function draw() {
+  clear();
+
   let r = map(mouseX, 0, width, 0, 255);
   let g = map(mouseX, 0, width, 50, 180);
   let b = map(mouseX, 0, width, 255, 80);
 
-  fill(r, g, b, 40);
+  fill(r, g, b, 12);
 
   rect(0, 0, width, height);
 }
