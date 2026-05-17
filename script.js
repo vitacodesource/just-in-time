37517a7c00b2fa5c03b6460a42a838da7eb6ca77
vitacodesource <vitaaguitar@gmail.com>
2026-05-17 15:31:20 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255, 0, 0);
+  let c = createCanvas(windowWidth, windowHeight);
+  c.position(0, 0);
+  c.style('position', 'fixed');
+  c.style('top', '0');
+  c.style('left', '0');
+  c.style('z-index', '10');
+  c.style('pointer-events', 'none');
+
+  background(0, 0, 0, 0);
 }
 
 function draw() {
+  clear();
+
   noStroke();
-  fill(0);
-  ellipse(mouseX, mouseY, 20);
+  fill(255, 180, 80, 120);
+  ellipse(mouseX, mouseY, 30);
 }
